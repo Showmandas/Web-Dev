2493dd295cae1787af42a8a6020184029d8154cc
Showman_Das <Showmandas3216@gmail.com>
2023-02-01 22:31:30 +0600

    determine rectangle area

---
+++ Javascript/functionProblems/rectangle_area.js
@@ -0,0 +1,7 @@
+function rectangle_area(l,h){
+let area=l*h
+return area
+}
+
+// console.log(rectangle_area(5,4))
+console.log(rectangle_area(10,6))
---
+++ Javascript/someArrayfunc.js
@@ -0,0 +1,14 @@
+var name=['jamila','kamila','ramila','pamila']
+
+// includes 
+// console.log(name.includes('ramila'))
+// console.log(name.includes('chamila'));
+
+// slice 
+
+// console.log(name.slice(0,3))
+
+// splice 
+
+console.log(name.splice(1,2));
+console.log(name)
