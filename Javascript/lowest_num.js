d20ac07432eca838a01f420ff8b0090f0f94a056
Showman_Das <Showmandas3216@gmail.com>
2023-02-01 23:14:04 +0600

    find lowesr number

---
+++ Javascript/lowest_num.js
@@ -0,0 +1,13 @@
+var n1=4
+var n2=3
+var n3=1
+
+if(n1<n2 && n1<n3){
+    console.log(n1,'is lowest')
+}
+else if(n2<n1 && n2<n3){
+    console.log(n2,'is lowest');
+}
+else{
+    console.log(n3,'is lowest');
+}
