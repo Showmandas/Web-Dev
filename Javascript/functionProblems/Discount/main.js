e67369ccea8ac5cbe623c8268318f1866bcacb76
Showman_Das <Showmandas3216@gmail.com>
2023-02-15 11:30:12 +0600

    text editor by js

--- Javascript/functionProblems/Discount/main.js
+++ Javascript/functionProblems/Discount/main.js
@@ -1,11 +1,25 @@
 let applybtn=document.getElementById('applybtn');
 
 applybtn.addEventListener('click',function(){
-    let price=document.getElementById('price').innerText;
+    //Easy Version
+    
+    // let price=document.getElementById('price').innerText;
+    // let priceval=parseFloat(price);
+    // let discval=priceval * 0.3;
+    // let finalPrice=priceval-discval;
+    // document.getElementById('pay').innerHTML=finalPrice;
+
+    // with coupon 
+
+    let couponCode=document.getElementById('couponCode').value;
+    if(couponCode == 'DISC30'){
+        let price=document.getElementById('price').innerText;
     let priceval=parseFloat(price);
     let discval=priceval * 0.3;
     let finalPrice=priceval-discval;
     document.getElementById('pay').innerHTML=finalPrice;
-
-    
+    }else{
+        alert('Wrong Coupon Code!! Price: Tk 1000');
+    }
+    document.getElementById('couponCode').value ="";
 })
