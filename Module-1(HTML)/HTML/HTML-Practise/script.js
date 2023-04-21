52d5a8174fc6bc73ab5d2ce7593b965ce440ac6a
Showman_Das <Showmandas3216@gmail.com>
2023-04-21 21:24:43 +0600

    simple js works

--- Module-1(HTML)/HTML/HTML-Practise/script.js
+++ Module-1(HTML)/HTML/HTML-Practise/script.js
@@ -1,13 +1,13 @@
 let bars = document.getElementById("Bars");
 let navItem = document.getElementById("navItem");
 let items = document.getElementById("items");
+// let header=document.getElementsByTagName('header');
 bars.addEventListener("click", () => {
   navItem.classList.toggle("active");
   items.classList.toggle("open");
   if (items.classList.contains("open")) {
     items.style.visibility = "visible";
-    // navItem.style.flexDirection='column';
-    // items.style.width='100%'
+   
   } else {
     items.style.visibility = "hidden";
   }
@@ -23,5 +23,6 @@ let aboutHeader=document.getElementById('aboutHeader');
 
 moon.addEventListener('click',()=>{
     main.classList.toggle('dark');
+    
     // aboutHeader.style.color='#b2bec3'
 })
