5d8a72adfa0e675cb112b61fd554b3d440f75cf1
Showman_Das <Showmandas3216@gmail.com>
2023-04-22 12:03:00 +0600

    created a bank

--- Tailwind/TailWind-Site/main.js
+++ Tailwind/TailWind-Site/main.js
@@ -1,83 +1,123 @@
-let Toggle=document.getElementById('Toggle');
-let text=document.getElementById('Texts');
-let ReadText=document.getElementById('ReadText');
-let textToggle=document.getElementById('textToggle');
-let toggleText=document.getElementById('toggleText');
-let bgcolor=document.querySelector('.bgcolor');
+let Toggle = document.getElementById('Toggle');
+let text = document.getElementById('Texts');
+let ReadText = document.getElementById('ReadText');
+let textToggle = document.getElementById('textToggle');
+let toggleText = document.getElementById('toggleText');
+let bgcolor = document.querySelector('.bgcolor');
 
 // criteria show 
-const toggle=()=>{
+const toggle = () => {
   text.classList.toggle('textbox');
   // textToggle.innerHTML="hide";
   // textToggle.style.backgroundColor="#ff2222"
-  if(textToggle.innerHTML == "hide"){
-    textToggle.innerHTML="show our criteria"
-  }else{
-    textToggle.innerHTML="hide"
+  if (textToggle.innerHTML == "hide") {
+    textToggle.innerHTML = "show our criteria"
+  } else {
+    textToggle.innerHTML = "hide"
   }
 }
 
 // dark and light theme 
-Toggle.addEventListener('click',()=>{
+Toggle.addEventListener('click', () => {
   bgcolor.classList.toggle('Bgcolor');
 })
 
 // read more and less 
-const readMore=()=>{
-ReadText.classList.toggle('textbox')
-// toggleText.style.display='none';
-if(toggleText.style.display=='none'){
-toggleText.style.display='block'
-}else{
-toggleText.style.display='none'
-}
+const readMore = () => {
+  ReadText.classList.toggle('textbox')
+  // toggleText.style.display='none';
+  if (toggleText.style.display == 'none') {
+    toggleText.style.display = 'block'
+  } else {
+    toggleText.style.display = 'none'
+  }
 }
 
 
 // show clock 
 
-const clockShow=()=>{
-    let time=new Date();
-    let h=time.getHours();
-    let m=time.getMinutes();
-    let s=time.getSeconds();
-    let ampm=document.getElementById('ampm');
-    let greetings=document.getElementById('greetings')
-    let greetDiv=document.getElementById('greetDiv')
-
-    
-    if(h>=12){
-        ampm.innerText="PM"
-    }else{
-        ampm.innerText="AM"
-    }
-    // if(h>12){
-    //     h=h-12
-    // }
-
-    if(h>=6 && h<12){
-        greetings.innerText="Good Morning"
-        greetings.style.color="#fff";
-        greetDiv.style.backgroundColor="#000";
-    }else if(h>=12 && h<=15){
-        greetings.innerText="Good Noon";
-        greetings.style.color="#f2f234";
-        greetDiv.style.backgroundColor="#000";
-    }else if(h>15 && h<20){
-        greetings.innerHTML="Good Evening";
-        greetings.style.color="#000"
-        greetDiv.style.backgroundColor="#F6BA6F"
-    }else{
-        greetings.innerText="Good Night";
-        greetings.style.color="#F6BA6F"
-        greetDiv.style.backgroundColor="#000"
-
-    }
+const clockShow = () => {
+  let time = new Date();
+  let h = time.getHours();
+  let m = time.getMinutes();
+  let s = time.getSeconds();
+  let ampm = document.getElementById('ampm');
+  let greetings = document.getElementById('greetings')
+  let greetDiv = document.getElementById('greetDiv')
 
 
-    document.getElementById('hours').innerText=h;
-    document.getElementById('minutes').innerText=m;
-    document.getElementById('seconds').innerText=s;
+  if (h >= 12) {
+    ampm.innerText = "PM"
+  } else {
+    ampm.innerText = "AM"
+  }
+  // if(h>12){
+  //     h=h-12
+  // }
+
+  if (h >= 6 && h < 12) {
+    greetings.innerText = "Good Morning"
+    greetings.style.color = "#fff";
+    greetDiv.style.backgroundColor = "#000";
+  } else if (h >= 12 && h <= 15) {
+    greetings.innerText = "Good Noon";
+    greetings.style.color = "#f2f234";
+    greetDiv.style.backgroundColor = "#000";
+  } else if (h > 15 && h < 20) {
+    greetings.innerHTML = "Good Evening";
+    greetings.style.color = "#000"
+    greetDiv.style.backgroundColor = "#F6BA6F"
+  } else {
+    greetings.innerText = "Good Night";
+    greetings.style.color = "#F6BA6F"
+    greetDiv.style.backgroundColor = "#000"
+
+  }
+
+
+  document.getElementById('hours').innerText = h;
+  document.getElementById('minutes').innerText = m;
+  document.getElementById('seconds').innerText = s;
 
 }
-setInterval(clockShow,1000)
+setInterval(clockShow, 1000)
+
+
+
+//bank 
+
+// for deposit 
+let depositBtn = document.getElementById('depositBtn');
+depositBtn.addEventListener('click', () => {
+  let net = document.getElementById('net');
+  let netVal = parseInt(net.innerText)
+
+  let depositInp = document.getElementById('depositInp');
+  let depositInpAmount = parseInt(depositInp.value);
+  let deposit = document.getElementById('deposit')
+  let depositVal=parseInt(deposit.innerText);
+  deposit.innerHTML = depositInpAmount + depositVal;
+  console.log(typeof depositInpAmount);
+  let netBalance = depositInpAmount + netVal;
+  console.log(typeof netBalance);
+  console.log(typeof netVal);
+  net.innerHTML = netBalance;
+  // console.log(depositInpAmount);
+})
+
+
+// for withdraw 
+let withdrawBtn=document.getElementById('withdrawBtn');
+
+withdrawBtn.addEventListener('click',()=>{
+  let net = document.getElementById('net');
+  let netVal = parseInt(net.innerText)
+
+  let withdrawInp=document.getElementById('withdrawInp');
+  let withdrawInpAmount=parseInt(withdrawInp.value);
+  let withdraw=document.getElementById('withdraw')
+  let withdrawVal=parseInt(withdraw.innerText);
+  withdraw.innerHTML=withdrawInpAmount + withdrawVal;
+  let netBalance = netVal - withdrawInpAmount;
+net.innerHTML=netBalance 
+})
